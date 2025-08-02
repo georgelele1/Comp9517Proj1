23ca2336dd720a6c0c78c42ff01b86e002d39f28
OLDLIU0925 <LYC2860053193@gmail.com>
2025-08-03 04:57:33 +1000

    Delete U_Net/paths.py

--- U_Net/paths.py
+++
@@ -1,5 +0,0 @@
-from glob import glob
-
-rgb_paths  = sorted(glob(r"C:\Users\28600\Desktop\COMP9517\Segementation_image\USA_segmentation\RGB_images\*.png"))
-nrg_paths  = sorted(glob(r"C:\Users\28600\Desktop\COMP9517\Segementation_image\USA_segmentation\NRG_images\*.png"))
-mask_paths = sorted(glob(r"C:\Users\28600\Desktop\COMP9517\Segementation_image\USA_segmentation\masks\*.png"))
